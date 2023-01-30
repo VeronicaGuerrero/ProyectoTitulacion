e2066cbe63820892b10ab5f2e72122b3a6fcf54b
VeronicaGuerrero <veri_bellas96@hotmail.com>
2023-01-29 21:56:08 -0500

    validaciones

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
 })
 app.use(express.static('views'))
-app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/eyeTracker.html')
 })
